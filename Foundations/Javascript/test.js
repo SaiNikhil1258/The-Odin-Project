9cadb51caf2fb01eb503ff91dbd180260b6d9008
Nikhil <sainikhil1258@gmail.com>
2024-02-23 23:38:42 +0530

    minor updates and js

---
+++ Foundations/Javascript/test.js
@@ -0,0 +1,20 @@
+// const button = document.querySelector('button')
+
+
+// function  greet() { 
+//     const name = prompt("what is your name")
+//     const greeting = document.querySelector("#greeting")
+//     greeting.textContent = `Hello ${name}, nice to see you`;
+    
+// }
+// button.addEventListener("click",greet)
+// const song = "Fight the Youth";
+// const score = 9;
+// const highestScore = 10;
+// const output = `I like the song ${song}. I gave it a score of ${(score / highestScore) * 100}%.`;
+// console.log(output); 
+
+const newline = "one day i will be writing backend like anything \n what would be the way to get there i dont know"
+
+
+console.log(newline)
